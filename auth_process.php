cdf3007b92dd89b85110aa889784796a6d3dc84b
devlucas <lukabril@hotmail.com>
2023-04-03 23:42:22 -0300

    adição

--- auth_process.php
+++ auth_process.php
@@ -1,22 +1,20 @@
-<?php 
+<?php
 
-require_once("globals.php");
-require_once("db.php");
-require_once("models/User.php");
-require_once("models/Message.php");
-require_once("dao/UserDAO.php");
+  require_once("globals.php");
+  require_once("db.php");
+  require_once("models/User.php");
+  require_once("models/Message.php");
+  require_once("dao/UserDAO.php");
 
-$message = new Message($BASE_URL);
+  $message = new Message($BASE_URL);
 
-$userDao = new UserDAO($conn, $BASE_URL);
+  $userDao = new UserDAO($conn, $BASE_URL);
 
-// Verifica o tipo do formulário
+  // Resgata o tipo do formulário
+  $type = filter_input(INPUT_POST, "type");
 
-$type = filter_input(INPUT_POST, "type");
-
-// Verificação do tipo de formulário
-
-if($type === "register") {
+  // Verificação do tipo de formulário
+  if($type === "register") {
 
     $name = filter_input(INPUT_POST, "name");
     $lastname = filter_input(INPUT_POST, "lastname");
@@ -24,50 +22,71 @@ if($type === "register") {
     $password = filter_input(INPUT_POST, "password");
     $confirmpassword = filter_input(INPUT_POST, "confirmpassword");
 
-    //Verificação de dados minimos
-    if($name && $lastname && $email && $password){
-        // Verificar se as senhas batem
-        if($password === $confirmpassword){
+    // Verificação de dados mínimos 
+    if($name && $lastname && $email && $password) {
 
-            //verificar se o email já está cadastrado no sistema
-            if($userDao->findByEmail($email) === false){
+      // Verificar se as senhas batem
+      if($password === $confirmpassword) {
 
-                $user = new User();
+        // Verificar se o e-mail já está cadastrado no sistema
+        if($userDao->findByEmail($email) === false) {
 
-                // Criação de token e senha
-                $userToken = $user->generateToken();
-                $finalPassword = $user->generatePassword($password);
+          $user = new User();
 
-                $user->name = $name;
-                $user->lastname = $lastname;
-                $user->email = $email;
-                $user->password = $finalPassword;
-                $user->token = $userToken;
-                
-                $auth = true;
+          // Criação de token e senha
+          $userToken = $user->generateToken();
+          $finalPassword = $user->generatePassword($password);
 
-                $userDao->create($user, $auth);
+          $user->name = $name;
+          $user->lastname = $lastname;
+          $user->email = $email;
+          $user->password = $finalPassword;
+          $user->token = $userToken;
 
-            }else{
+          $auth = true;
 
-                //Enviar uma mensagem de erro, usuário já existe
-                $message->setMessage("Usuário já cadastrado, tente outro email.", "erro", "back");
-            }
+          $userDao->create($user, $auth);
 
-        }else
+        } else {
+          
+          // Enviar uma msg de erro, usuário já existe
+          $message->setMessage("Usuário já cadastrado, tente outro e-mail.", "error", "back");
 
-            //Enviar uma mensagem de erro, de senhas não batem
-            $message->setMessage("As senhas não são iguais.", "erro", "back");
+        }
 
-    }else{
+      } else {
 
-        //Enviar uma mensagem de erro, de dados faltantes
-        $message->setMessage("Por favor, preencha todos os campos.", "erro", "back");
+        // Enviar uma msg de erro, de senhas não batem
+        $message->setMessage("As senhas não são iguais.", "error", "back");
+
+      }
+
+    } else {
+
+      // Enviar uma msg de erro, de dados faltantes
+      $message->setMessage("Por favor, preencha todos os campos.", "error", "back");
+
     }
 
-}else if($type === "login"){
+  } else if($type === "login") {
 
+    $email = filter_input(INPUT_POST, "email");
+    $password = filter_input(INPUT_POST, "password");
 
-}
+    // Tenta autenticar usuário
+    if($userDao->authenticateUser($email, $password)) {
 
-?>
+      $message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+
+    // Redireciona o usuário, caso não conseguir autenticar
+    } else {
+
+      $message->setMessage("Usuário e/ou senha incorretos.", "error", "back");
+
+    }
+
+  } else {
+
+    $message->setMessage("Informações inválidas!", "error", "index.php");
+
+  }
